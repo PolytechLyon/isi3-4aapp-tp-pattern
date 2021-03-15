15622ffaf0a8e3a190f4b7f9e0e175a4d045b877
laetitiamatignon <laetitia.matignon@univ-lyon1.fr>
2021-03-15 16:52:51 +0100

    mise a jour JUnit

--- src/strategybeers/HappyHourTest.java
+++ src/strategybeers/HappyHourTest.java
@@ -1,7 +1,7 @@
 package strategybeers;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class HappyHourTest {
 
